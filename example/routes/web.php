a03f147c1553d21ed1cfa2c8793fd71c3c5f5721
Dariia Sliusar <dariiasliusar@gmail.com>
2025-01-08 12:18:11 +0100

    second

--- example/routes/web.php
+++ example/routes/web.php
@@ -1,15 +1,57 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', [
+    return view('home');
+});
 
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Dyrector',
+                'salary' => '$50,0000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Developer',
+                'salary' => '$40,0000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$30,0000'
+            ],
+        ]
     ]);
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Dyrector',
+            'salary' => '$50,0000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Developer',
+            'salary' => '$40,0000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '$30,0000'
+        ],
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
+
 });
 
 Route::get('/contact', function () {
