b45f7f97fa31823cd5c51754139de6f3dc7ce466
Dariia Sliusar <dariiasliusar@gmail.com>
2025-01-11 22:31:18 +0100

    Meet Eloquent

--- example/app/Models/Job.php
+++ example/app/Models/Job.php
@@ -2,38 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,0000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Developer',
-                'salary' => '$40,0000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$30,0000'
-            ],
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-        return $job;
-    }
+class Job extends Model
+{
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary'];
 }
 
